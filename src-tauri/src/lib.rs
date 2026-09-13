f7a5543108302a860f5f57b1d7721c152c0e3592
Maksym Marchuk <maxxik007@gmail.com>
2026-09-13 10:40:56 +0200

    build: configured release artifacts signature, added updater plugin and corresponding keys to be used in build job env

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,6 +21,7 @@ pub fn run() {
     let repository_manager = RepositoryManager::new(&launch_dir);
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_http::init())
